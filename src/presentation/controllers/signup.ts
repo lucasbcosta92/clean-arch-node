dc383ebdbf69144960e0a33b0fbf8619f4af7411
lucasbcosta92 <lucasbcosta92@gmail.com>
2025-06-24 18:40:59 -0300

    feat: ensure SignUpController.handle implement correct interface

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,7 @@
+import { httpRequest, HttpResponse } from "@presentation/protocols/http"
+
 export class SignUpController {
-  handle(httpRequest: any): any {
+  handle(httpRequest: httpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
