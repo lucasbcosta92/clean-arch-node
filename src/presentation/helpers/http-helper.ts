1977a0abed8db48c5006a2d4951dd3dfbfdecec5
lucasbcosta92 <lucasbcosta92@gmail.com>
2025-06-25 19:52:44 -0300

    refactor: move all protocol imports to a single file

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,4 @@
-import { HttpResponse } from "../protocols/http"
-
+import { HttpResponse } from "../protocols"
 import { ServerError } from "../errors"
 
 export const badRequest = (error: Error): HttpResponse => ({
