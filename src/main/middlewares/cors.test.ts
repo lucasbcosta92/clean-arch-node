1bc2952b6d3aa405ceaf2dbbd009bfbb40127468
lucasbcosta92 <lucasbcosta92@gmail.com>
2025-06-29 13:34:34 -0300

    test: ensure cors is enable

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import request from 'supertest'
+
+import app from '../config/app'
+
+describe('Cors Middleware', () => {
+  it('should enable cors', async () => {
+    app.post('/test-cors', (req: Request, res: Response) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test-cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  })
+})
