75200ef9c0abefd4399f05d568f79c6b68e85043
lucasbcosta92 <lucasbcosta92@gmail.com>
2025-07-01 18:05:46 -0300

    refactor: connect to mongofb before integration tests

--- src/main/routes/singup-routes.test.ts
+++ src/main/routes/singup-routes.test.ts
@@ -2,7 +2,23 @@ import request from 'supertest'
 
 import app from '../config/app'
 
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
+
 describe('Signup routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  });
+
+  beforeEach(async () => {
+    const accountsCollection = MongoHelper.getCollection('accounts')
+    // Remove all accounts
+    accountsCollection.deleteMany({})
+  })
+
   it('should return an account on success', async () => {
     await request(app)
       .post('/api/signup').send({
