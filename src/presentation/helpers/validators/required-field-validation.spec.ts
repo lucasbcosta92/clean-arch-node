f186601aabafe3e420ce79cd902b7121d562fefe
lucasbcosta92 <lucasbcosta92@gmail.com>
2025-07-09 13:43:05 -0300

    test: ensure RequiredFieldValidation not return if validation succeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,4 +1,5 @@
-import { MissingParamError } from "@presentation/errors"
+import { MissingParamError } from "../../errors"
+
 import { RequiredFieldValidation } from "./"
 
 describe('Required Field Validation', () => {
@@ -8,5 +9,13 @@ describe('Required Field Validation', () => {
     const error = sut.validate({ another: 'another' })
 
     expect(error).toEqual(new MissingParamError('name'))
+  })
+
+  it('should not return if validation succeds', () => {
+    const sut = new RequiredFieldValidation('name')
+
+    const error = sut.validate({ name: 'name' })
+
+    expect(error).toBeFalsy()
   })
 })
