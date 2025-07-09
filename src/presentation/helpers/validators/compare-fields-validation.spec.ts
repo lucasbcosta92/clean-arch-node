b1b8bb4999a4eebb9174ab9ebc8065ae65bec0f4
lucasbcosta92 <lucasbcosta92@gmail.com>
2025-07-09 13:48:10 -0300

    test: add tests to CompareFieldsValidation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,25 @@
+import { InvalidParamError } from "../../errors"
+
+import { CompareFieldsValidation } from "./"
+
+const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation => {
+  return new CompareFieldsValidation(field, fieldToCompare)
+}
+
+describe('Compare Fields Validation', () => {
+  it('should return an error a InvalidParamError if validation fails', () => {
+    const sut = makeSut('password', 'passwordConfirmation')
+
+    const error = sut.validate({ password: 'password', passwordConfirmation: 'another_password' })
+
+    expect(error).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+
+  it('should not return if validation succeds', () => {
+    const sut = makeSut('password', 'passwordConfirmation')
+
+    const error = sut.validate({ password: 'password', passwordConfirmation: 'password' })
+
+    expect(error).toBeFalsy()
+  })
+})
