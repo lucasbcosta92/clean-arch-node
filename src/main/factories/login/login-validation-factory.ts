d1cf08439dab6d7e6e15e755266c10e28cbc20ee
lucasbcosta92 <lucasbcosta92@gmail.com>
2025-07-14 19:02:01 -0300

    feat: add login route

--- src/main/factories/login/login-validation-factory.ts
+++ src/main/factories/login/login-validation-factory.ts
@@ -15,7 +15,6 @@ export const makeLoginValidation = (): ValidationComposite => {
     validations.push(new RequiredFieldValidation(field))
   }
 
-  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
   validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
 
   return new ValidationComposite(validations)
